67a2d7a4ad5c7806437b8b1fc3dd615867d143f8
brycen <brycenbb1996@gmail.com>
2022-06-26 21:32:20 +0100

    Diary shows way to use the user object without making my own state zzzzz

--- src/components/Diary/index.js
+++ src/components/Diary/index.js
@@ -2,11 +2,14 @@ import { useEffect, useState } from 'react';
 import Header from '../Header';
 import Tag from '../../Assets/Tag.png';
 import SimpleAccordion from '../Accordian';
+import { useAuth0 } from '@auth0/auth0-react';
+
 // import NotesForm from '../NotesForm/index.js';
 /*Props: user{email} -> Email used to locate the users notes in the server */
 function Diary(props) {
   const [notes, setNotes] = useState([]);
-
+  const user = useAuth0();
+  console.log(user);
   /*
   SideEffect that runs on load to display all of the notes of the current logged in user 
   This component is protected from being loaded if there is no current user
