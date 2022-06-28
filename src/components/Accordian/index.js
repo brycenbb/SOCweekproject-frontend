da7495fe642b07b06f4db593b4b6ec6b4bbfe56b
brycen <brycensoc@gmail.com>
2022-06-28 11:26:37 +0100

    "happy to help' box is broken in back end

--- src/components/Accordian/index.js
+++ src/components/Accordian/index.js
@@ -20,7 +20,7 @@ export default function SimpleAccordion(props) {
     <main id="allNotesContainer">
       {filteredArr.map(function (itemArr, index) {
         if (itemArr.length === 0) {
-          return <></>;
+          return <div key={index}> </div>;
         }
         return (
           <Accordion key={index} sx={{ border: '1px solid #181e50' }}>
